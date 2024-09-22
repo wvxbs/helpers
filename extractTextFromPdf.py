1b219fc7fef8efa96a9bccf3893174d9d11ab4ee
wvxbs <gabriel.ferreira7854@gmail.com>
2024-09-22 15:33:13 -0300

    helpers para a extração de texto de pdf e pptx adicionads

---
+++ extractTextFromPdf.py
@@ -0,0 +1,24 @@
+import pdfplumber
+import os
+from dotenv import load_dotenv
+
+def extractTextFromPdf(inputFile):
+    with pdfplumber.open(inputFile) as pdf:
+        textContent = []
+        for page in pdf.pages:
+            text = page.extract_text()
+            if text:
+                textContent.append(text)
+    return "\n".join(textContent)
+
+load_dotenv()
+
+pdfFile = os.getenv("PDF_FILE")
+outputFile = os.getenv("PDF_OUTPUT_FILE")
+
+plainText = extractTextFromPdf(pdfFile)
+
+with open(outputFile, "w", encoding="utf-8") as f:
+    f.write(plainText)
+
+print("Exportação de PDF para texto concluída!")
---
+++ extractTextFromPptx.py
@@ -0,0 +1,24 @@
+import os
+from pptx import Presentation
+from dotenv import load_dotenv
+
+def extractTextFromPptx(inputFile):
+    prs = Presentation(inputFile)
+    textContent = []
+    for slide in prs.slides:
+        for shape in slide.shapes:
+            if hasattr(shape, "text"):
+                textContent.append(shape.text)
+    return "\n".join(textContent)
+
+load_dotenv()
+
+pptxFile = os.getenv("PPTX_FILE")
+outputFile = os.getenv("PPTX_OUTPUT_FILE")
+
+plainText = extractTextFromPptx(pptxFile)
+
+with open(outputFile, "w", encoding="utf-8") as f:
+    f.write(plainText)
+
+print("Exportação concluída!")
